4993e2827fcf5d80f8908011389e1344e8aeccf2
Shin <sirapop1998@gmail.com>
2025-09-15 10:04:09 +0700

    feat/question-4/5

--- question-5.js
+++ question-5.js
@@ -2,5 +2,6 @@
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
 Answer:
 
+O(n^2) because the function is using nested loops to sort the products by price.
 
 */
